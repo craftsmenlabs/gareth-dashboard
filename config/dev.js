a62f06311dac272791075e1631a320b5321b66b9
Hylke Stapersma <hylke.stapersma@gmail.com>
2015-10-13 21:33:52 +0200

    Preparation for new REST interface

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 var config =  {
+    "backendUrl" : "http://localhost:8080",
     "backendExperimentUrl" : "http://localhost:8080/experiments"
 }
--- public/js/experiments.js
+++ public/js/experiments.js
@@ -1,5 +1,16 @@
 angular.module('experimentApp', []).controller('ExperimentController', function ($scope, $http) {
     $http.get(config.backendExperimentUrl).success(function (data) {
-        $scope.experiments = data;
+        $scope.experiments = [];
+
+        data.forEach(function (experiment, i) {
+            $http.get(experiment._links[0].href).success(function (experimentRun) {
+                experimentRun.forEach(function(er){
+                    var experimentCopy = angular.copy(experiment);
+                    $scope.experiments.push(angular.merge(experimentCopy, er));
+                });
+            });
+        });
+
+        console.log($scope.experiments);
     });
 });
