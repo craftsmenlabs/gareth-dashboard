da732545225b53e46276445ddc8e07faaf961f21
Hylke Stapersma <hylke.stapersma@gmail.com>
2015-10-30 11:23:49 +0100

    Rerun reloads the dataset

--- public/js/experiments.js
+++ public/js/experiments.js
@@ -1,14 +1,25 @@
 angular.module('experimentApp', []).controller('ExperimentController', function ($scope, $http) {
-    $http.get(config.backendExperimentUrl).success(function (data) {
-        $scope.experiments = [];
 
-        data.forEach(function (experiment, i) {
-            $http.get(experiment._links[0].href).success(function (experimentRun) {
-                experimentRun.forEach(function(er){
-                    var experimentCopy = angular.copy(experiment);
-                    $scope.experiments.push(angular.merge(experimentCopy, er));
+    function init() {
+        $http.get(config.backendExperimentUrl).success(function (data) {
+            $scope.experiments = [];
+
+            data.forEach(function (experiment, i) {
+                $http.get(experiment._links[0].href).success(function (experimentRun) {
+                    experimentRun.forEach(function (er) {
+                        var experimentCopy = angular.copy(experiment);
+                        $scope.experiments.push(angular.merge(experimentCopy, er));
+                    });
                 });
             });
         });
-    });
+    }
+
+    init();
+
+    $scope.rerun = function (rerunLink) {
+        $http.get(rerunLink).success(function () {
+            init();
+        });
+    }
 });
