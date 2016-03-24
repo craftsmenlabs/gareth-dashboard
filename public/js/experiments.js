57292a32b782c108ed38c7cb83fa26212c8763f2
Jasper Sprengers <j.sprengers@portofrotterdam.com>
2016-03-24 14:16:04 +0100

    added piecharts for experiments and runs

--- public/js/experiments.js
+++ public/js/experiments.js
@@ -1,26 +1,46 @@
-angular.module('experimentApp', []).controller('ExperimentController', function ($scope, $http) {
+angular.module('experimentApp', []).controller('ExperimentController', function ($scope, $http, $q) {
 
 
     var config = {};
+    var allRuns = 0;
+    var failureRuns = 0;
 
 
     function init() {
         if (config.backendExperimentUrl) {
             $http.get(config.backendExperimentUrl).success(function (data) {
                 $scope.experiments = [];
-
+                var promises = [];
                 data.forEach(function (experiment, i) {
-                    $http.get(experiment._links[0].href).success(function (experimentRun) {
-                        experimentRun.forEach(function (er) {
-                            var experimentCopy = angular.copy(experiment);
-                            $scope.experiments.push(angular.merge(experimentCopy, er));
-                        });
-                    });
+                    promises.push($http.get(experiment._links[0].href))
+                });
+                $q.all(promises).then(function (responses) {
+                    processBatchResponse(data, responses);
+                    drawChart("#experimentChart", allRuns, failureRuns);
+                    drawChart("#runChart", allRuns, failureRuns);
                 });
             });
         } else {
             throw new Error("Needs experiment backend URL");
         }
+    }
+
+    function processBatchResponse(experiments, responses) {
+        experiments.forEach(function (experiment, i) {
+            var runs = responses[i].data;
+            console.log(runs)
+            parseRunsForExperiment(experiment, runs);
+        });
+    }
+
+    function parseRunsForExperiment(experiment, experimentRuns) {
+        console.log(experimentRuns.length + " runs in experiment " + experiment.experiment_name)
+        experimentRuns.forEach(function (er) {
+            allRuns = allRuns + 1;
+            var experimentCopy = angular.copy(experiment);
+            failureRuns += (er.failure_execution ? 1 : 0);
+            $scope.experiments.push(angular.merge(experimentCopy, er));
+        });
     }
 
     function loadConfig() {
