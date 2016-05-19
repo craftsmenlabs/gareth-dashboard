ef362db861e9dc4f5c0c0fb279a671c4e7d482c1
David Hardy <david.hardy@codecentric.nl>
2016-05-19 15:30:58 +0200

    Started on autocomplete for baseline

--- app/scripts/controllers/createexperiment.js
+++ app/scripts/controllers/createexperiment.js
@@ -8,20 +8,16 @@
  * Controller of the garethApp
  */
 angular.module('garethApp')
-  .controller('CreateexperimentCtrl', function ($scope, $http) {
+  .controller('CreateexperimentCtrl', function ($scope, $http, $q) {
     $scope.experiment = {};
 
     $scope.create = function () {
-      // var result = JSON.parse($scope.jsonString);
       console.log(JSON.stringify($scope.experiment));
-      // console.log(JSON.stringify(experiment2));
-
       $http.post("http://localhost:8080/definitions", $scope.experiment
       ).then(handleSuccess, handleError('Error executing command'));
     };
 
     function handleSuccess(res) {
-      console.log(JSON.stringify(res));
       return res.data;
     }
 
@@ -30,5 +26,44 @@ angular.module('garethApp')
         return {success: false, message: error};
       };
     }
+
+    $scope.baseline = {};
+    $scope.baseline.querySearch = queryDefinitions;
+
+    // md-selected-item="baseline.selectedItem"
+    // md-search-text="baseline.searchText"
+    // querySearch
+
+    function queryDefinitions(type, query) {
+      var deferred = $q.defer();
+
+      function deferredQueryDefinitions() {
+        if (query && query.length <= 0) {
+          return [];
+        }
+
+        $http.get("http://localhost:8080/definitions/" + type + "/" + query).then(handleSuccess, handleError('Error executing command')).then(function (definition) {
+          console.log(JSON.stringify(definition));
+          var results = [];
+
+          if (definition.suggestions && definition.suggestions.length > 0) {
+            results = definition.suggestions.map(function (suggestion) {
+              return {
+                value: suggestion,
+                display: suggestion
+              };
+            });
+          }
+
+          deferred.resolve(results);
+        });
+      }
+
+      _.debounce(deferredQueryDefinitions, 100, true)();
+
+      return deferred.promise;
+    }
+
+
   })
 ;
