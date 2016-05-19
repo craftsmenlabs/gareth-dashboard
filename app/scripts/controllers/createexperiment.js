c1fb37ffa30c17ec2eb11b9294d217b40a86594d
David Hardy <david.hardy@codecentric.nl>
2016-05-19 14:18:33 +0200

    Initial version of creating new experiments via the front end

--- app/scripts/controllers/createexperiment.js
+++ app/scripts/controllers/createexperiment.js
@@ -8,10 +8,25 @@
  * Controller of the garethApp
  */
 angular.module('garethApp')
-  .controller('CreateexperimentCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('CreateexperimentCtrl', function ($scope, $http) {
+    $scope.jsonString = "";
+
+    $scope.apply = function () {
+      var result = JSON.parse($scope.jsonString);
+
+        $http.post("http://localhost:8080/definitions", result
+      ).then(handleSuccess, handleError('Error executing command'));
+    };
+
+    function handleSuccess(res) {
+      console.log(JSON.stringify(res));
+      return res.data;
+    }
+
+    function handleError(error) {
+      return function () {
+        return {success: false, message: error};
+      };
+    }
+  })
+;
